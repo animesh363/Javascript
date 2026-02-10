b13781fa31e2511c8db68e03a0bf0fd6bfd2043c
animesh363 <animeshanand86033@gmail.com>
2026-02-10 13:49:59 +0530

    till oprator

--- pepJs/file1.js
+++ pepJs/file1.js
@@ -1,9 +1,9 @@
 console.log("Hello Duniya")
 
-let a=5;
-let b=6;
-let c= a + b;
-console.log("a + b = " + c)
+// let a=5;
+// let b=6;
+// let c= a + b;
+// console.log("a + b = " + c)
 
 //comments: 
 //multiline conmment
@@ -12,5 +12,42 @@ console.log("line 2")
 console.log("line 3")
 */
 
+// name ="John"
+// console.log("Hello " + name)
+// console.log(`Hello ${name}`)
 
+
+//clg->console.log
+
+
+
+//Oprators
+let a=10
+let b = 5
+
+// console.log("Sun = " + a+b)
+// console.log("diff = " + (a-b))
+// console.log("mul = " + a*b)
+// console.log("div = " + a/b)
+// console.log("modulo " + a%b)
+
+//Comparoson oprator
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a>=b);
+
+
+// let x ="10"
+// let y=10
+
+// console.log(x==y); //comare value only
+// console.log(x===y);// compare value and data type
+
+
+//Logical oprators
+// console.log(a>b && a<b); //both concn should be true
+// console.log(a>b || a<b); //any one should be true
+
+console.log(!a>b);
 
