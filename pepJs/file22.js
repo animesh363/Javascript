6fc2dbf207879b25386f7640a2bf18bd4081319e
animesh363 <animeshanand86033@gmail.com>
2026-02-16 10:50:32 +0530

    file24

---
+++ pepJs/file22.js
@@ -0,0 +1,11 @@
+function response(){
+    console.log(user);
+}
+
+const user={
+    name:"John",
+    email:"john@gmail.com",
+    role:"user",
+};
+
+response (user)
---
+++ pepJs/file23.js
@@ -0,0 +1,13 @@
+function response({name, email, role}){
+    console.log(name);
+    console.log(email);
+    console.log(role);
+}
+
+const user={
+    name:"John",
+    email:"john@gmail.com",
+    role:"user",
+};
+
+response (user)
