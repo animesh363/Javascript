5123db5c6999f58ca0af87b14e488a1529be4bad
animesh363 <animeshanand86033@gmail.com>
2026-02-16 13:45:01 +0530

    hoisting

--- pepJs/file27.js
+++ pepJs/file27.js
@@ -1,14 +1,23 @@
-let d = new Date()
-console.log(d);
+// let d = new Date()
+// console.log(d);
 
-//get method
-console.log(d.getDate());
-console.log(d.getMonth());
-console.log(d.getFullYear());
-console.log(d.getHours());
-console.log(d.getMinutes());
-console.log(d.getSeconds());
-console.log(d.getMilliseconds());
-console.log(d.getTime());
-console.log(d.getDay());
-console.log(Date.now());
+// //get method
+// console.log(d.getDate());
+// console.log(d.getMonth());
+// console.log(d.getFullYear());
+// console.log(d.getHours());
+// console.log(d.getMinutes());
+// console.log(d.getSeconds());
+// console.log(d.getMilliseconds());
+// console.log(d.getTime());
+// console.log(d.getDay());
+// console.log(Date.now());
+
+
+//crating manul date
+
+// let d = new Date (2026,1,1,20,12,10) //(year, month, day, hours, minutes, seconds)
+// console.log(d);
+
+const myDate = new Date(2025, 1, 16);
+console.log(myDate);
---
+++ pepJs/file28.js
@@ -0,0 +1,19 @@
+//Hoisting
+
+//Var: Hoisted + initialized with undefined
+// var name
+// name="Animesh"
+// console.log(name);
+
+
+///hoisting with let and const
+
+// let name
+// name ="Animesh"
+// console.log(name);
+
+
+
+//const → Same as let but cannot reassign
+// console.log(name);
+// const name ="Animesh"
