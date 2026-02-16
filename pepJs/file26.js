a9ea41f73bc07398804435a30c7423f543122fff
animesh363 <animeshanand86033@gmail.com>
2026-02-16 11:40:29 +0530

    getmethod

---
+++ pepJs/file26.js
@@ -0,0 +1,11 @@
+let str ="Hello"
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str.indexOf("l"));
+// console.log(str.includes("o"));
+
+console.log(str.startsWith("H"));//true.false
+console.log(str.endsWith("o"));//true.false
+
+console.log();
---
+++ pepJs/file27.js
@@ -0,0 +1,14 @@
+let d = new Date()
+console.log(d);
+
+//get method
+console.log(d.getDate());
+console.log(d.getMonth());
+console.log(d.getFullYear());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+console.log(d.getMilliseconds());
+console.log(d.getTime());
+console.log(d.getDay());
+console.log(Date.now());
